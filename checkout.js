fccecf4845ebea08877575924e8ae68fedad24a6
S0URABHshukla <sourabh01itsme@gmail.com>
2022-11-29 03:48:43 +0530

    process

--- checkout.js
+++ checkout.js
@@ -4,3 +4,16 @@ document.querySelector("#cross").addEventListener("click",hideCheckoutPopup);
         const hide=document.querySelector("#checkout-details");
         hide.style.display="none"
     }
+document.querySelector("#qnty").addEventListener("change",qntty);
+function qntty()
+{
+    var qty=document.querySelector("#qnty").value;
+    if(qty===1)
+    {
+        document.querySelector("#total-price").innerHTML=10*qty;
+    }
+    else if(qty===2)
+    {
+        document.querySelector("#total-price").innerHTML=20*qty;
+    }
+}    
