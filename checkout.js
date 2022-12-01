9dd566895338093ef3f6c442d158a49712558bf7
S0URABHshukla <sourabh01itsme@gmail.com>
2022-12-01 16:29:13 +0530

    acc dummy done

--- checkout.js
+++ checkout.js
@@ -1,19 +1,28 @@
+let totalPrice=Json.parse(localStorage.getItem("totalPrice"))|| 0;
 document.querySelector("#cross").addEventListener("click",hideCheckoutPopup);
     function hideCheckoutPopup()
     {
         const hide=document.querySelector("#checkout-details");
         hide.style.display="none"
     }
-document.querySelector("#qnty").addEventListener("change",qntty);
+
 function qntty()
 {
-    var qty=document.querySelector("#qnty").value;
-    if(qty===1)
+    var qty=document.querySelector("#qntty").value;
+    var totalprice=document.querySelector("#price").innerHTML;
+    if(qty==1)
     {
-        document.querySelector("#total-price").innerHTML=10*qty;
+        document.querySelector("#total-price").textContent=qty*totalprice;
     }
-    else if(qty===2)
+    else if(qty==2)
     {
-        document.querySelector("#total-price").innerHTML=20*qty;
+        document.querySelector("#total-price").innerHTML=qty*totalprice;
     }
+    else if(qty==3)
+    {
+        document.querySelector("#total-price").innerHTML=qty*totalprice;
+    }
+    localStorage.setItem("totalPrice",JSON.stringify(qty*totalprice));
+    document.querySelector("#pr").innerHTML=`Rs. ${qty*totalprice}`;
+    document.querySelector("#price-button").innerHTML=`PROCEED TO PAY Rs. ${qty*totalprice}`;
 }    
